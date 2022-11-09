472b49e020410504efc087a10468ea6ad4475102
sarahemilly <110680621+sarahemilly@users.noreply.github.com>
2022-11-09 17:45:16 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,10 +1,10 @@
 
-
 // alert("Oi! Eu sou o Alert");
 // document.write("Estou na página.");
 // console.log("Eu estou no console.");
 let tabuada = 5;
 function alo(){
+
     let tabuada = document.getElementById("entrada").value;
 
     document.write("<h1>Tabuada do " + tabuada + "</h1>");
@@ -18,19 +18,24 @@ function alo(){
     document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
     document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10));
-       
 }
+// for(inicio, validaçao, manutençao)
 function escreva(){
-  for(var i=1; i <= 10; i++){
-     for(var j = 1; j <=10; j++){
-document.write(i + " x " + j + " = " + (j*i)+"<br>");
-     }
-    document.write("<br>");
- }
-
+    for(var i=1; i <= 10; i++){
+        for(var j = 1; j <=10; j++){
+            document.write(i + " x " + j + " = " + (j*i)+"<br>");
+        }
+        document.write("<br>");
+    }
 }
-function moeda(atual){
-    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+function quadrado(){
+    for(var i = 2; i < 11; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
+    }
+}
+function moeda (atual){
+  return  atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
 function total(){
     let val = document.getElementById("valor").value;
@@ -38,13 +43,10 @@ function total(){
     
     let t = document.getElementById("meses").value;
     let resultado = 0;
-    let saida = "";
     for(let m = 1; m <= t; m++){
         resultado = (val * ((ju/100)+1));
         val = resultado;
-        saida += "Mês " + m + ": " + moeda(val) + "<br>";
-        //document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
+        document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
     }
-    document.getElementById("mes").innerHTML = saida;
-    document.getElementById("resultado").innerHTML + "Total: " + moeda(resultado);
-    //document.write("O total é de: " + moeda(resultado));
+    document.write("O total é de: " + moeda(resultado) );
+}
